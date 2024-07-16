d5b778342fc66b2a054912c56c6e002fec2791a5
SkyClerik <SkyClerik@bk.ru>
2024-07-16 12:52:16 +0300

    SuperUpdate
    Эффекты и greyBox

---
+++ Assets/Gameplay/Camera/CameraBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class CameraBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed = 1;
+        [SerializeField]
+        public VariableJoystick _variableJoystick;
+
+        private Vector3 _direction;
+
+        public void FixedUpdate()
+        {
+            if (_variableJoystick.Direction != Vector2.zero)
+            {
+                _direction = Vector3.forward * _variableJoystick.Vertical + Vector3.right * _variableJoystick.Horizontal;
+                _direction.y = 0;
+                transform.Translate(_direction * _speed * Time.fixedDeltaTime, Space.World);
+            }
+        }
+    }
+}
